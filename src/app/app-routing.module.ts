ba2d8edddcdbffaaa7bfa33d84f8a5d3de0ee5e5
manikandanr2712 <rmanikandan901@gmail.com>
2022-12-05 14:56:53 +0530

    Pages changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FirstComponent } from './first/first.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 {
-    path: '',
-    loadChildren: () => import('./feature-module-example/feature-module-example.module').then(m => m.FeatureModuleExampleModule)
-  }
+    path: 'login',
+    // loadChildren: () => import('./parent/parent.module').then(m => m.ParentModule)
+    component: FirstComponent
+  },
+  // { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'home', component: HomeComponent , data: {animation: 'Home'} },
+  { path: 'about', component: AboutComponent , data: {animation: 'About'} },
+  { path: 'contact', component: ContactComponent , data: {animation: 'Contact'} },
 ];
 
 @NgModule({
